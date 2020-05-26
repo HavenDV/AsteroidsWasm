54fd7876e41247c9c62615a8285ce162f3618e5f
Konstantin S <havendv@gmail.com>
2020-05-26 20:41:40 +1000

    Added simple Avalonia implementation

---
+++ Asteroids.Avalonia/Controls/GraphicContainer.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Asteroids.Standard.Enums;
+using Asteroids.Standard.Interfaces;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Immutable;
+using Brushes = Avalonia.Media.Brushes;
+using Color = System.Drawing.Color;
+using Point = Avalonia.Point;
+
+namespace Asteroids.Avalonia.Controls
+{
+    public class GraphicContainer : Canvas, IGraphicContainer
+    {
+        #region Properties
+
+        // Pen is IDisposable
+        private IReadOnlyDictionary<DrawColor, Color> ColorCache { get; set; }
+        private IEnumerable<IGraphicLine> LastLines { get; set; } = new List<IGraphicLine>();
+        private IEnumerable<IGraphicPolygon> LastPolygons { get; set; } = new List<IGraphicPolygon>();
+
+        #endregion
+
+        #region Methods
+
+        public Task Initialize(IReadOnlyDictionary<DrawColor, string> drawColorMap)
+        {
+            ColorCache = new ReadOnlyDictionary<DrawColor, Color>(
+                drawColorMap.ToDictionary(
+                    kvp => kvp.Key
+                    , kvp => (Color)(new ColorConverter().ConvertFromString(kvp.Value) ?? Colors.White)
+                )
+            );
+
+            return Task.CompletedTask;
+        }
+
+        public Task Draw(IEnumerable<IGraphicLine> lines, IEnumerable<IGraphicPolygon> polygons)
+        {
+            InvalidateVisual();
+            LastLines = lines;
+            LastPolygons = polygons;
+
+            return Task.CompletedTask;
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override void Render(DrawingContext context)
+        {
+            base.Render(context);
+
+            if (ColorCache == null)
+            {
+                return;
+            }
+
+            foreach (var line in LastLines)
+            {
+                context.DrawLine(
+                    new ImmutablePen(Brushes.AliceBlue), 
+                    new Point(line.Point1.X, line.Point1.Y), 
+                    new Point(line.Point2.X, line.Point2.Y));
+            }
+            foreach (var poly in LastPolygons)
+            {
+                context.DrawGeometry(
+                    Brushes.AliceBlue,
+                    new ImmutablePen(Brushes.AliceBlue),
+                    new PolylineGeometry(poly.Points.Select(i => new Point(i.X, i.Y)), true));
+            }
+        }
+
+        #endregion
+    }
+}
--- Asteroids.Avalonia/MainWindow.xaml.cs
+++ Asteroids.Avalonia/MainWindow.xaml.cs
@@ -1,22 +1,142 @@
-﻿using Avalonia;
+﻿using System;
+using System.Drawing;
+using Asteroids.Standard;
+using Asteroids.Standard.Enums;
+using Asteroids.Standard.Interfaces;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+
+#nullable enable
 
 namespace Asteroids.Avalonia
 {
     public class MainWindow : Window
     {
+        #region Properties
+
+        private IGameController Controller { get; }
+
+        #endregion
+
+        #region Constructors
+
         public MainWindow()
         {
             InitializeComponent();
+            
 #if DEBUG
             this.AttachDevTools();
 #endif
+
+            Controller = new GameController();
+            Opened += OnOpened;
+            KeyDown += OnHandler;
+            KeyUp += OnEventHandler;
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
+
+        #endregion
+
+        #region Event Handlers
+
+        private async void OnOpened(object? _, EventArgs args)
+        {
+            await Controller.Initialize(this.FindControl<Controls.GraphicContainer>("MainContainer"), new Rectangle(0, 0, (int)Width, (int)Height));
+        }
+
+        private void OnHandler(object? _, KeyEventArgs args)
+        {
+            PlayKey key;
+            switch (args.Key)
+            {
+                case Key.Escape:
+                    // Escape during a title screen exits the game
+                    if (Controller.GameStatus == GameMode.Title)
+                    {
+                        Close();
+                        return;
+                    }
+
+                    key = PlayKey.Escape;
+                    break;
+
+                case Key.Left:
+                    key = PlayKey.Left;
+                    break;
+
+                case Key.Right:
+                    key = PlayKey.Right;
+                    break;
+
+                case Key.Up:
+                    key = PlayKey.Up;
+                    break;
+
+                case Key.Down:
+                    key = PlayKey.Down;
+                    break;
+
+                case Key.Space:
+                    key = PlayKey.Space;
+                    break;
+
+                case Key.P:
+                    key = PlayKey.P;
+                    break;
+
+                default:
+                    return;
+            }
+
+            Controller.KeyDown(key);
+        }
+
+        private void OnEventHandler(object? _, KeyEventArgs args)
+        {
+            PlayKey key;
+            switch (args.Key)
+            {
+                case Key.Escape:
+                    key = PlayKey.Escape;
+                    break;
+
+                case Key.Left:
+                    key = PlayKey.Left;
+                    break;
+
+                case Key.Right:
+                    key = PlayKey.Right;
+                    break;
+
+                case Key.Up:
+                    key = PlayKey.Up;
+                    break;
+
+                case Key.Down:
+                    key = PlayKey.Down;
+                    break;
+
+                case Key.Space:
+                    key = PlayKey.Space;
+                    break;
+
+                case Key.P:
+                    key = PlayKey.P;
+                    break;
+
+                default:
+                    return;
+            }
+
+            Controller.KeyUp(key);
+        }
+
+        #endregion
     }
 }
