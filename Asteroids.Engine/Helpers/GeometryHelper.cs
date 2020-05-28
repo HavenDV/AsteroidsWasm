b957782ce4b0f51d8fa915549885015705040df8
Konstantin S <havendv@gmail.com>
2020-05-28 11:41:29 +1000

    Moved GeometryHelper

--- Asteroids.Engine/Helpers/GeometryHelper.cs
+++ Asteroids.Engine/Helpers/GeometryHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
-namespace Asteroids.Standard.Helpers
+namespace Asteroids.Engine.Helpers
 {
     /// <summary>
     /// Helpers for performing Geometric calculations.
